51be5538d7218d4f885a3c6610e85ecefe1f20b6
Serge <serge@droganov.ru>
2016-07-06 23:08:48 +0300

    react-15 upgrade

--- src/index.jsx
+++ src/index.jsx
@@ -2,76 +2,71 @@
 import React, { Component } from "react";
 
 export default class Svg extends Component {
-   propTypes: {
+   static propTypes = {
       center: React.PropTypes.bool,
       width: React.PropTypes.number.isRequired,
       height: React.PropTypes.number,
       minWidth: React.PropTypes.number,
       maxWidth: React.PropTypes.number,
-   }
-   constructor(props) {
-      super(props);
-      // this.state = {};
-      this.defaultProps = {
-         center: false,
-      };
-      // this.statics = {};
-   }
+   };
+   static defaultProps = { center: false };
    render() {
+     const {
+       center,
+       width,
+       height: propsHeight,
+       minWidth: propsMinWidth,
+       maxWidth: propMaxWidth,
+       ...htmlProps,
+     } = this.props;
+
       // variables
-      const width = this.props.width;
-      const height = this.props.height ? this.props.height : width;
-      const minWidth = this.props.minWidth || 0;
-      const maxWidth = this.props.maxWidth || width;
+      const height = propsHeight ? propsHeight : width;
+      const minWidth = propsMinWidth || 0;
+      const maxWidth = propMaxWidth || width;
 
       // container styling
       const containerStyle = {
-         maxWidth: maxWidth + "px",
-         minWidth: minWidth + "px",
+         maxWidth: `${maxWidth}px`,
+         minWidth: `${minWidth}px`,
       };
 
-      if ( this.props.center === "true" || this.props.center === true ) {
-         containerStyle.margin = "auto";
-      }
+      if (center === 'true' || center === true) containerStyle.margin = 'auto';
+
       // wrap styling
       const wrapStyle = {
          height: 0,
-         paddingBottom: height / width * 100 + "%",
-         position: "relative",
-         width: "100%",
+         paddingBottom: `${height / width * 100}%`,
+         position: 'relative',
+         width: '100%',
       };
 
       // svg props
       const svgInjectProps = {
-         preserveAspectRatio:"xMinYMin meet",
+         preserveAspectRatio: 'xMinYMin meet',
          width: null,
          height: null,
          style: {
-            position: "absolute",
+            position: 'absolute',
             top: 0,
             left: 0,
             bottom:0,
             right:0,
-            display: "block",
-            fill: "currentColor",
-            // width: "100%",
-            // height: "100%",
+            display: 'block',
+            fill: 'currentColor',
+            // width: '100%',
+            // height: '100%',
          },
       };
       const svgProps = Object.assign( {}, this.props.children.props, svgInjectProps );
-
+      console.log( 'htmlProps', htmlProps )
       return (
          <div
-            { ...this.props }
-            style={ Object.assign( {}, this.props.style, containerStyle ) }
-            center={ null }
-            width={ null }
-            height={ null }
+            {...htmlProps}
+            style={Object.assign({}, this.props.style, containerStyle)}
          >
-            <div
-               style={ wrapStyle }
-            >
-               <svg { ...svgProps } />
+            <div style={ wrapStyle }>
+               <svg {...svgProps} />
             </div>
          </div>
       );
